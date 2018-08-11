ccf7478032d7675b8efeed48a13bf0dfdea0f223
Bhushan Bapat <bhushan_bapat29@yahoo.co.in>
2018-08-11 17:48:25 +0530

    deleted unnecessary files

--- AppNo1/ViewController.swift
+++ AppNo1/ViewController.swift
@@ -10,14 +10,98 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    let uname1 : UITextField = {
+        let uname = UITextField()
+        uname.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        uname.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        uname.backgroundColor = .white
+        uname.placeholder = "Username"
+        uname.borderStyle = UITextBorderStyle.bezel
+        return uname
+        }()
+    let pass : UITextField = {
+        let pass = UITextField()
+        pass.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        pass.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        pass.backgroundColor = .white
+        pass.placeholder = "Password"
+        pass.isSecureTextEntry = true
+        pass.borderStyle = UITextBorderStyle.bezel
+        return pass
+        }()
+    
+    let submit : UIButton = {
+        let submit = UIButton()
+        submit.backgroundColor = .white
+        submit.setTitle("Submit", for: UIControlState.normal)
+        submit.setTitleColor(.black, for: UIControlState.normal)
+        submit.backgroundColor = .red
+        submit.addTarget(self, action: #selector(changeColor), for: UIControlEvents.touchUpInside)
+        return submit
+        }()
+    
+    let inputContainerView : UIView = {
+        let iC = UIView()
+        iC.backgroundColor = .white
+        iC.translatesAutoresizingMaskIntoConstraints = false
+        iC.layer.cornerRadius = 10
+        iC.layer.masksToBounds = true
+        return iC
+    }()
+    
+    let back : UIImageView = {
+        let b = UIImageView()
+        b.image = UIImage(named: "back")
+        b.contentMode = .scaleAspectFill
+        b.translatesAutoresizingMaskIntoConstraints = false
+        return b
+    }()
+    
+    @objc func changeColor(sender : UIButton!){
+        //let newViewController = NavViewController()
+        present(UINavigationController(rootViewController: ViewController1()), animated: true, completion: nil)
+        
+//        back.image = UIImage(named: "background1")
+//        back.contentMode = .scaleAspectFill
+//        back.translatesAutoresizingMaskIntoConstraints = false
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(back)
+        view.addSubview(inputContainerView)
+        back.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        back.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+        
+        inputContainerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        inputContainerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        inputContainerView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -30).isActive = true
+        inputContainerView.heightAnchor.constraint(equalToConstant: 120).isActive = true
+        
+        
+        let stackView = UIStackView(arrangedSubviews: [uname1, pass, submit])
+        stackView.axis = .vertical
+        stackView.distribution = .fillProportionally
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        inputContainerView.addSubview(stackView)
+        stackView.topAnchor.constraint(equalTo : inputContainerView.topAnchor).isActive=true
+        stackView.centerXAnchor.constraint(equalTo: inputContainerView.centerXAnchor).isActive = true
+        
+        uname1.widthAnchor.constraint(equalTo: inputContainerView.widthAnchor).isActive = true
+        uname1.heightAnchor.constraint(equalTo: inputContainerView.heightAnchor, multiplier: 1/3).isActive = true
+        
+        pass.widthAnchor.constraint(equalTo: inputContainerView.widthAnchor).isActive = true
+        pass.heightAnchor.constraint(equalTo: inputContainerView.heightAnchor, multiplier: 1/3).isActive = true
+        
+        
+        
+        submit.widthAnchor.constraint(equalTo: inputContainerView.widthAnchor).isActive = true
+        submit.heightAnchor.constraint(equalTo: inputContainerView.heightAnchor, multiplier: 1/3).isActive = true
+        
+        
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
--- AppNo1/ViewController1.swift
+++ AppNo1/ViewController1.swift
@@ -10,26 +10,111 @@ import UIKit
 
 class ViewController1: UIViewController {
 
+    let tableView : UITableView = {
+        let tableView = UITableView()
+        tableView.contentMode = .scaleAspectFill
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    let textView: UITextView = {
+       let textView = UITextView()
+        textView.text = "Hello World"
+        textView.font = UIFont.boldSystemFont(ofSize: 30)
+        textView.contentMode = .scaleAspectFill
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
+        
+    }()
+    
+    let leftView : UIView = {
+        let view = UIView()
+        view.backgroundColor = .red
+        view.contentMode = .scaleAspectFill
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let rightView : UIView = {
+        let view = UIView()
+        view.backgroundColor = .blue
+        view.contentMode = .scaleAspectFill
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let slider : UISlider = {
+        let slider = UISlider()
+        slider.backgroundColor = .cyan
+        slider.contentMode = .scaleAspectFill
+        slider.translatesAutoresizingMaskIntoConstraints = false
+        slider.setValue(0.5, animated: true)
+        slider.addTarget(self, action: #selector(changeSize), for: .valueChanged)
+        return slider
+    }()
+    var leftConst = NSLayoutConstraint()
+    var rightConst = NSLayoutConstraint()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(goBack))
+        
+        view.backgroundColor = .white
+        view.addSubview(leftView)
+        view .addSubview(rightView)
+        leftView.addSubview(tableView)
+        rightView.addSubview(textView)
+        
+        tableView.topAnchor.constraint(equalTo: leftView.topAnchor).isActive = true
+        tableView.leftAnchor.constraint(equalTo: leftView.leftAnchor).isActive = true
+        tableView.widthAnchor.constraint(equalTo: leftView.widthAnchor).isActive = true
+        tableView.heightAnchor.constraint(equalTo: leftView.heightAnchor).isActive = true
+        
+        textView.topAnchor.constraint(equalTo: rightView.topAnchor).isActive = true
+        textView.leftAnchor.constraint(equalTo: rightView.leftAnchor).isActive = true
+        textView.widthAnchor.constraint(equalTo: rightView.widthAnchor).isActive = true
+        textView.heightAnchor.constraint(equalTo: rightView.heightAnchor).isActive = true
+        
+        
+        view.addSubview(slider)
+        
+        slider.topAnchor.constraint(equalTo: view.topAnchor, constant: 70).isActive = true
+        slider.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        slider.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        
+        leftView.topAnchor.constraint(equalTo: slider.bottomAnchor).isActive = true
+        leftView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        leftView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        leftConst = leftView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1/2)
+        leftConst.isActive = true;
+        
 
+        rightView.topAnchor.constraint(equalTo: slider.bottomAnchor).isActive = true
+        rightView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        rightView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        rightConst = rightView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1/2)
+        rightConst.isActive = true
+        
+        
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @objc func changeSize(_ sender: UISlider!){
+        let x = slider.value
+        let y = 1 - x
+        leftConst.isActive = false
+       // rightView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: CGFloat(y)).isActive = true
+        leftConst = leftView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: CGFloat(x))
+        leftConst.isActive = true
+        
+        rightConst.isActive = false
+        rightConst = rightView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: CGFloat(y))
+            rightConst.isActive = true
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @objc func goBack(){
+        let vc = ViewController()
+        present(vc, animated: true, completion: nil)
     }
-    */
 
 }
